fef50f0f8d5e9b94b9f3cc3689ec110b6a59a75c
index1207 <dev.index@outlook.com>
2026-02-08 13:29:44 +0900

    RpcService 추가

---
+++ example/example.cpp
@@ -0,0 +1,11 @@
+﻿#include <Raven/Framework/WebApplication/RpcService.hpp>
+
+#include  <crow.h>
+
+int main() {
+    crow::App<> app;
+    raven::RpcService rpcSvc;
+    rpcSvc.RegisterRpcEndpoint(app);
+
+    app.port(5000).multithreaded().run();
+}
---
+++ include/Raven/Framework/WebApplication/RpcService.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include <memory>
+
+#include <crow/app.h>
+
+namespace raven {
+    class RpcService {
+    public:
+        RpcService();
+        ~RpcService();
+    public:
+        template<typename... Middlewares>
+        void RegisterRpcEndpoint(crow::App<Middlewares...>& app) {
+            auto& bp = *_bp;
+            app.register_blueprint(bp);
+        }
+    private:
+        std::unique_ptr<crow::Blueprint> _bp;
+    };
+}
